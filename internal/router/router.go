6e654375a487e48250bdefb7e0d2da39daa441c3
vinylhousegarage <vinylhousegarage@gmail.com>
2025-07-16 00:12:56 +0900

    feat(router): add initial router.go to internal/router

---
+++ internal/router/router.go
@@ -0,0 +1,19 @@
+package router
+
+import (
+    "idpproxy/internal/system/health"
+
+    "github.com/gin-gonic/gin"
+
+    "go.uber.org/zap"
+)
+
+func NewRouter(logger *zap.Logger) *gin.Engine {
+    r := gin.New()
+    r.Use(gin.Recovery())
+
+    system := r.Group("")
+    health.RegisterRoutes(system, logger)
+
+    return r
+}
