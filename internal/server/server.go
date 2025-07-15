c4ee28e87b0dd5387fa9dec9fab5ec07e1f533b6
vinylhousegarage <vinylhousegarage@gmail.com>
2025-07-16 00:41:58 +0900

    feat(server): add initial server.go to internal/server

---
+++ internal/server/server.go
@@ -0,0 +1,17 @@
+package server
+
+import (
+	"idpproxy/internal/config"
+
+	"go.uber.org/zap"
+)
+
+func StartServer(r *gin.Engine, logger *zap.Logger) {
+	port := config.GetPort()
+
+	logger.Info("Starting server", zap.String("port", port))
+
+	if err := r.Run(":" + port); err != nil {
+			logger.Fatal("Server failed", zap.Error(err))
+	}
+}
