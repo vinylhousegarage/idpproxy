b804272ddd538793df40574d277a20af33a3b7f1
vinylhousegarage <vinylhousegarage@gmail.com>
2025-07-16 16:48:33 +0900

    test(root): add unit test for root endpoint handler

---
+++ internal/system/root/handler_test.go
@@ -0,0 +1,32 @@
+package root
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
+
+	"go.uber.org/zap"
+)
+
+func TestNewRootHandler_Returns200JSON(t *testing.T) {
+	t.Parallel()
+
+	gin.SetMode(gin.TestMode)
+	router := gin.Default()
+
+	handler := NewRouteHandler(zap.NewNop())
+	router.GET("/", handler.Serve)
+
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+
+	require.Equal(t, http.StatusOK, w.Code)
+	require.JSONEq(t, `{
+		"message": "Welcome to IdP Proxy",
+		"openapi": "https://idpproxy.com/openapi.json"
+	}`, w.Body.String())
+}
