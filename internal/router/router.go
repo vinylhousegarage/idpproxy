381e3017bd24296a4fe5dbee1fe3c0b0038e7678
vinylhousegarage <vinylhousegarage@gmail.com>
2025-07-17 20:49:41 +0900

    refactor(router): inject dependencies and fix group registration

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,21 +1,31 @@
 package router
 
 import (
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+
+	"github.com/vinylhousegarage/idpproxy/internal/auth/google/login"
+	"github.com/vinylhousegarage/idpproxy/internal/config"
+	"github.com/vinylhousegarage/idpproxy/internal/httpclient"
 	"github.com/vinylhousegarage/idpproxy/internal/system/health"
 	"github.com/vinylhousegarage/idpproxy/internal/system/root"
-
-	"github.com/gin-gonic/gin"
-
-	"go.uber.org/zap"
 )
 
-func NewRouter(logger *zap.Logger) *gin.Engine {
+func NewRouter(
+	logger *zap.Logger,
+	metadataURL string,
+	cfg config.GoogleConfig,
+	client httpclient.HTTPClient,
+) *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Recovery())
 
-	system := r.Group("")
-	health.RegisterRoutes(system, logger)
-	root.RegisterRoutes(system, logger)
+	googleGroup := r.Group("google")
+	login.RegisterRoutes(googleGroup, metadataURL, cfg, client, logger)
+
+	systemGroup := r.Group("")
+	health.RegisterRoutes(systemGroup, logger)
+	root.RegisterRoutes(systemGroup, logger)
 
 	return r
 }
