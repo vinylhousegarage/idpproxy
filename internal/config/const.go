6de8ddcace13fc8d652e0ae0b43e8ff321f03d59
vinylhousegarage <vinylhousegarage@gmail.com>
2025-07-17 13:57:54 +0900

    feat(config): add constants for Google OAuth config

--- internal/config/const.go
+++ internal/config/const.go
@@ -1,3 +1,10 @@
 package config
 
 const GoogleOIDCMetadataURL = "https://accounts.google.com/.well-known/openid-configuration"
+
+const (
+	GoogleResponseType = "code"
+	GoogleScope        = "openid email profile"
+	GoogleAccessType   = "offline"
+	GooglePrompt       = "consent"
+)
