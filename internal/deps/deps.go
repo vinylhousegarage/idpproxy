16a6ff40c90a26a643dc252b366cd170f81e3863
vinylhousegarage <vinylhousegarage@gmail.com>
2025-07-19 02:00:43 +0900

    refactor(deps): inject FirestoreClient and HTTPClient via args; move Firestore init to main

--- internal/deps/deps.go
+++ internal/deps/deps.go
@@ -3,6 +3,7 @@ package deps
 import (
 	"net/http"
 
+	"cloud.google.com/go/firestore"
 	"go.uber.org/zap"
 
 	"github.com/vinylhousegarage/idpproxy/internal/config"
@@ -10,17 +11,25 @@ import (
 )
 
 type Dependencies struct {
-	MetadataURL string
-	Config      *config.GoogleConfig
-	HTTPClient  httpclient.HTTPClient
-	Logger      *zap.Logger
+	MetadataURL     string
+	Config          *config.GoogleConfig
+	HTTPClient      httpclient.HTTPClient
+	FirestoreClient *firestore.Client
+	Logger          *zap.Logger
 }
 
-func New(metadataURL string, cfg *config.GoogleConfig, logger *zap.Logger) *Dependencies {
+func New(
+	metadataURL string,
+	googleConfig *config.GoogleConfig,
+	httpClient httpclient.HTTPClient,
+	firestoreClient *firestore.Client,
+	logger *zap.Logger,
+) *Dependencies {
 	return &Dependencies{
-		MetadataURL: metadataURL,
-		Config:      cfg,
-		HTTPClient:  &http.Client{},
-		Logger:      logger,
+		MetadataURL:     metadataURL,
+		Config:          googleConfig,
+		HTTPClient:      httpClient,
+		FirestoreClient: firestoreClient,
+		Logger:          logger,
 	}
 }
