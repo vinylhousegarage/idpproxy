da5f9b943ed254c966a9416fc35df235297ad938
vinylhousegarage <vinylhousegarage@gmail.com>
2025-07-20 22:45:02 +0900

    test(callback): add TestGenerateSessionID

---
+++ internal/oauth/google/callback/utils_test.go
@@ -0,0 +1,17 @@
+package callback
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenerateSessionID(t *testing.T) {
+	id := GenerateSessionID()
+
+	require.NotEmpty(t, id, "UUID should not be empty")
+
+	uuidRegex := regexp.MustCompile(`^[a-f0-9]{8}-[a-f0-9]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[a-f0-9]{12}$`)
+	require.True(t, uuidRegex.MatchString(id), "UUID format is invalid")
+}
