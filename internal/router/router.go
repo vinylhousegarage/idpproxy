be56bdb6261ab0ade4c592730a4231b4221f5fdf
vinylhousegarage <vinylhousegarage@gmail.com>
2025-07-21 22:09:37 +0900

    refactor(router): change static route from /public to /

--- internal/router/router.go
+++ internal/router/router.go
@@ -15,7 +15,7 @@ func NewRouter(di *deps.Dependencies, publicFS http.FileSystem) *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Recovery())
 
-	r.StaticFS("/public", publicFS)
+	r.StaticFS("/", publicFS)
 
 	googleGroup := r.Group("google")
 	login.RegisterRoutes(googleGroup, di)
