ce5e6c7517797997d738a4473ad0b907d8355d76
vinylhousegarage <vinylhousegarage@gmail.com>
2025-07-22 13:50:47 +0900

    chore(firebase): update InitFirebaseApp for using GOOGLE_APPLICATION_CREDENTIALS

--- internal/deps/firebase.go
+++ internal/deps/firebase.go
@@ -1,16 +1,14 @@
-package deps
-
 import (
 	"context"
-	firebase "firebase.google.com/go/v4"
-	"google.golang.org/api/option"
+
+	"firebase.google.com/go/v4"
 )
 
-func InitFirebaseApp(jsonPath string) (*firebase.App, error) {
-	opt := option.WithCredentialsFile(jsonPath)
-	app, err := firebase.NewApp(context.Background(), nil, opt)
+func InitFirebaseApp() (*firebase.App, error) {
+	app, err := firebase.NewApp(context.Background(), nil)
 	if err != nil {
 		return nil, err
 	}
+
 	return app, nil
 }
