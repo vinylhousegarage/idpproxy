d07d71cd8eaa5627f94f62298b43073e10a0f259
vinylhousegarage <vinylhousegarage@gmail.com>
2025-07-22 16:23:27 +0900

    feat(deps): add NewFirebaseApp and NewFirestoreClient to initialize Firebase and Firestore

--- internal/deps/firebase.go
+++ internal/deps/firebase.go
@@ -1,14 +1,29 @@
+package deps
+
 import (
 	"context"
 
-	"firebase.google.com/go/v4"
+	"cloud.google.com/go/firestore"
+	firebase "firebase.google.com/go/v4"
+	"go.uber.org/zap"
 )
 
-func InitFirebaseApp() (*firebase.App, error) {
+func NewFirebaseApp() (*firebase.App, error) {
 	app, err := firebase.NewApp(context.Background(), nil)
 	if err != nil {
 		return nil, err
 	}
 
 	return app, nil
+}
+
+func NewFirestoreClient(app *firebase.App, logger *zap.Logger) (*firestore.Client, error) {
+	ctx := context.Background()
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		logger.Error("failed to initialize Firestore via Firebase App", zap.Error(err))
+		return nil, err
+	}
+
+	return client, nil
 }
