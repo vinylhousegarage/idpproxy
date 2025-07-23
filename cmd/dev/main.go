03cc3e11a242d7935687d8269e785047a98eea7f
vinylhousegarage <vinylhousegarage@gmail.com>
2025-07-23 19:23:48 +0900

    refactor(main): remove Firestore and token repository dependencies

--- cmd/dev/main.go
+++ cmd/dev/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"net/http"
 
 	"go.uber.org/zap"
@@ -25,28 +24,10 @@ func main() {
 		}
 	}()
 
-	ctx := context.Background()
-
-	app, err := deps.NewFirebaseApp(ctx)
-	if err != nil {
-		logger.Fatal("failed to initialize Firebase App", zap.Error(err))
-	}
-
-	firestoreClient, err := deps.NewFirestoreClient(ctx, app, logger)
-	if err != nil {
-		logger.Fatal("failed to initialize Firestore client", zap.Error(err))
-	}
-
-	googleConfig, err := config.LoadGoogleConfig()
-	if err != nil {
-		logger.Fatal("failed to load google config", zap.Error(err))
-	}
-
 	metadataURL := config.GoogleOIDCMetadataURL
 	httpClient := &http.Client{}
-	tokenRepo := repository.NewGoogleTokenRepository(firestoreClient, logger)
 
-	di := deps.New(metadataURL, googleConfig, httpClient, tokenRepo, logger)
+	di := deps.New(metadataURL, httpClient, logger)
 
 	r := router.NewRouter(di, http.FS(public.PublicFS))
 
