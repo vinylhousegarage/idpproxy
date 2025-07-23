dd7ecb462527e550f5ed6e0d30e930c6c7451d38
vinylhousegarage <vinylhousegarage@gmail.com>
2025-07-23 20:08:55 +0900

    test(helpers): restore test_helpers; remove GoogleConfig mock from NewMockDeps

---
+++ test/testhelpers/test_helpers.go
@@ -0,0 +1,17 @@
+package testhelpers
+
+import (
+	"net/http"
+
+	"go.uber.org/zap"
+
+	"github.com/vinylhousegarage/idpproxy/internal/deps"
+)
+
+func NewMockDeps(logger *zap.Logger) *deps.Dependencies {
+	return &deps.Dependencies{
+		MetadataURL: "https://accounts.google.com/.well-known/openid-configuration",
+		HTTPClient: http.DefaultClient,
+		Logger:     logger,
+	}
+}
