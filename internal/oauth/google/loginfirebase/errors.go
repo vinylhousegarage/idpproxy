a73e0d4df1c2523d4dbaaa0cf70e32318b264b56
vinylhousegarage <vinylhousegarage@gmail.com>
2025-07-26 20:03:02 +0900

    feat(loginfirebase): add error constants

---
+++ internal/oauth/google/loginfirebase/errors.go
@@ -0,0 +1,12 @@
+package loginfirebase
+
+import (
+	"net/http"
+
+	"github/vinylhousegarage/idpproxy/internal/apperror"
+)
+
+var (
+	ErrInvalidIDToken = apperror.New(http.StatusUnauthorized, "invalid id_token") // 401 Unauthorized
+	ErrInvalidRequest = apperror.New(http.StatusBadRequest, "invalid request")    // 400 Bad Request
+)
