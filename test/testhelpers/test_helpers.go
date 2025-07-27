a4147afb99fc60577578e3c1d8fc39e4e0a4d4de
vinylhousegarage <vinylhousegarage@gmail.com>
2025-07-27 16:41:17 +0900

    test(testhelpers): add mock for GoogleDependencies with stubbed Verifier

--- test/testhelpers/test_helpers.go
+++ test/testhelpers/test_helpers.go
@@ -2,11 +2,20 @@ package testhelpers
 
 import (
 	"context"
+	"net/http"
 
 	"go.uber.org/zap"
+
+	"github.com/vinylhousegarage/idpproxy/internal/deps"
 )
 
-var MockLogger = zap.NewNop()
+func NewMockSystemDeps(logger *zap.Logger) *deps.SystemDependencies {
+	return &deps.SystemDependencies{
+		MetadataURL: "https://accounts.google.com/.well-known/openid-configuration",
+		HTTPClient:  http.DefaultClient,
+		Logger:      logger,
+	}
+}
 
 type MockVerifier struct {
 	VerifyFunc func(ctx context.Context, idToken string) (*auth.Token, error)
@@ -14,4 +23,15 @@ type MockVerifier struct {
 
 func (m *MockVerifier) VerifyIDToken(ctx context.Context, idToken string) (*auth.Token, error) {
 	return m.VerifyFunc(ctx, idToken)
+}
+
+func NewMockGoogleDeps(logger *zap.Logger) *deps.GoogleDependencies {
+	return &deps.GoogleDependencies{
+		Logger: logger,
+		Verifier: &MockVerifier{
+			VerifyFunc: func(ctx context.Context, idToken string) (*auth.Token, error) {
+				return &auth.Token{UID: "test-user"}, nil
+			},
+		},
+	}
 }
