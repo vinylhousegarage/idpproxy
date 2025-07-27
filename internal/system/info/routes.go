2a06c173c37b73a78355f72a7060de062f748894
vinylhousegarage <vinylhousegarage@gmail.com>
2025-07-27 20:30:33 +0900

    refactor(router): change *gin.IRoutes to gin.IRoutes in RegisterRoutes functions

--- internal/system/info/routes.go
+++ internal/system/info/routes.go
@@ -6,7 +6,7 @@ import (
 	"github.com/vinylhousegarage/idpproxy/internal/deps"
 )
 
-func RegisterRoutes(r *gin.IRoutes, systemDeps *deps.SystemDependencies) {
+func RegisterRoutes(r gin.IRoutes, systemDeps *deps.SystemDependencies) {
 	h := NewInfoHandler(systemDeps.Logger)
 	r.GET("/info", h.Serve)
 }
