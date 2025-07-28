f39f2b08ad03a209b4a0367968442d3cf42eb893
vinylhousegarage <vinylhousegarage@gmail.com>
2025-07-28 19:35:07 +0900

    feat(me): add handler and error constant

--- internal/oauth/google/me/errors.go
+++ internal/oauth/google/me/errors.go
@@ -7,6 +7,7 @@ import (
 )
 
 var (
-	ErrInvalidAuthorizationHeaderFormat = apperror.New(http.StatusBadRequest, "invalid authorization header format") // 400 Bad Request
-	ErrMissingAuthorizationHeader       = apperror.New(http.StatusUnauthorized, "missing authorization header")      // 401 Unauthorized
+	ErrFailedToWriteUserResponse        = apperror.New(http.StatusInternalServerError, "failed to write user response") // 500 Internal Server Error
+	ErrInvalidAuthorizationHeaderFormat = apperror.New(http.StatusBadRequest, "invalid authorization header format")    // 400 Bad Request
+	ErrMissingAuthorizationHeader       = apperror.New(http.StatusUnauthorized, "missing authorization header")         // 401 Unauthorized
 )
