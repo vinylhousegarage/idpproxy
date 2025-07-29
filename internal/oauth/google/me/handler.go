4d1f409f54501c2c72099b0f01448ea07ccf14fc
vinylhousegarage <vinylhousegarage@gmail.com>
2025-07-29 12:59:38 +0900

    fix(me): use gin.Context in Serve function

--- internal/oauth/google/me/handler.go
+++ internal/oauth/google/me/handler.go
@@ -25,27 +25,27 @@ func NewMeHandler(
 	}
 }
 
-func (h *MeHandler) Serve(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Headers", "Authorization")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
-	w.Header().Set("Vary", "Origin")
-	w.Header().Set("Allow", "GET, OPTIONS")
+func (h *MeHandler) Serve(c *gin.Context) {
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "Authorization")
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+	c.Writer.Header().Set("Vary", "Origin")
+	c.Writer.Header().Set("Allow", "GET, OPTIONS")
 
-	if r.Method == http.MethodOptions {
-		w.WriteHeader(http.StatusNoContent)
+	if c.Request.Method == http.MethodOptions {
+		c.Status(http.StatusNoContent)
 		return
 	}
 
-	idToken, err := ExtractAuthHeaderToken(r)
+	idToken, err := ExtractAuthHeaderToken(c.Request)
 	if err != nil {
-		httperror.WriteErrorResponse(w, err, h.Logger)
+		httperror.WriteErrorResponse(c.Writer, err, h.Logger)
 		return
 	}
 
-	token, err := verify.VerifyIDToken(r.Context(), h.Verifier, idToken)
+	token, err := verify.VerifyIDToken(c.Request.Context(), h.Verifier, idToken)
 	if err != nil {
-		httperror.WriteErrorResponse(w, err, h.Logger)
+		httperror.WriteErrorResponse(c.Writer, err, h.Logger)
 		return
 	}
 
@@ -56,9 +56,5 @@ func (h *MeHandler) Serve(w http.ResponseWriter, r *http.Request) {
 		Exp: int64(token.Claims["exp"].(float64)),
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(resp); err != nil {
-		h.Logger.Error("failed to write user response", zap.Error(err))
-		httperror.WriteErrorResponse(w, ErrFailedToWriteUserResponse, h.Logger)
-	}
+	c.JSON(http.StatusOK, resp)
 }
