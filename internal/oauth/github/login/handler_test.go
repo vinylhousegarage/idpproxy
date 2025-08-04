b3fb7d177d5d29cd8fc6544d6272ca402dc9d4fe
vinylhousegarage <vinylhousegarage@gmail.com>
2025-08-04 13:43:10 +0900

    test(github/login): replace handler integration test with unit test

--- internal/oauth/github/login/handler_test.go
+++ internal/oauth/github/login/handler_test.go
@@ -20,23 +20,17 @@ func TestGitHubLoginHandler_Serve(t *testing.T) {
 	dependencies := testhelpers.NewMockGitHubDeps(logger)
 	handler := NewGitHubLoginHandler(dependencies)
 
-	router := http.NewServeMux()
-	router.HandleFunc("/github/login", func(w http.ResponseWriter, r *http.Request) {
-		c, _ := gin.CreateTestContext(w)
-		c.Request = r
-		handler.Serve(c)
-	})
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+	c.Request, _ = http.NewRequest(http.MethodGet, "/github/login", nil)
 
-	server := httptest.NewServer(router)
-	defer server.Close()
+	handler.Serve(c)
+	res := w.Result()
+	defer res.Body.Close()
 
-	resp, err := http.Get(server.URL + "/github/login")
-	require.NoError(t, err)
-	defer resp.Body.Close()
+	require.Equal(t, http.StatusFound, res.StatusCode)
 
-	require.Equal(t, http.StatusFound, resp.StatusCode)
-
-	location := resp.Header.Get("Location")
+	location := res.Header.Get("Location")
 	require.NotEmpty(t, location)
 	require.True(t, strings.HasPrefix(location, "https://github.com/login/oauth/authorize?"))
 	require.Contains(t, location, "client_id=test-client-id")
@@ -45,11 +39,12 @@ func TestGitHubLoginHandler_Serve(t *testing.T) {
 	require.Contains(t, location, "allow_signup=false")
 	require.Contains(t, location, "state=")
 
-	cookies := resp.Cookies()
+	cookies := res.Cookies()
 	foundStateCookie := false
 	for _, c := range cookies {
 		if c.Name == "oauth_state" && c.HttpOnly && c.Secure {
 			foundStateCookie = true
+			break
 		}
 	}
 	require.True(t, foundStateCookie, "oauth_state cookie should be set and secure")
