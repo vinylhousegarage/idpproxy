53a5dcd93c0111b78588b5cec7d8ff3d4c4bcb03
vinylhousegarage <vinylhousegarage@gmail.com>
2025-08-04 13:23:34 +0900

    refactor(deps): add config to GitHubDependencies and update constructor

--- internal/deps/github.go
+++ internal/deps/github.go
@@ -2,14 +2,18 @@ package deps
 
 import (
 	"go.uber.org/zap"
+
+	"github.com/vinylhousegarage/idpproxy/internal/config"
 )
 
 type GitHubDependencies struct {
+	Config *config.GitHubConfig
 	Logger *zap.Logger
 }
 
-func NewGitHubDeps(logger *zap.Logger) *GitHubDependencies {
+func NewGitHubDeps(cfg *config.GitHubConfig, logger *zap.Logger) *GitHubDependencies {
 	return &GitHubDependencies{
+		Config: cfg,
 		Logger: logger,
 	}
 }
