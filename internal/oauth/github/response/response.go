e1c04a1b8d7cf551f5008ebf11a98c044e3e4030
vinylhousegarage <vinylhousegarage@gmail.com>
2025-08-11 11:38:49 +0900

    style(goimports): apply goimports to fix formatting error

--- internal/oauth/github/response/response.go
+++ internal/oauth/github/response/response.go
@@ -1,23 +1,23 @@
 package response
 
 type AuthResultResponse struct {
-    Sub      string `json:"sub"`
-    Provider string `json:"provider"`
-    Status   int    `json:"status"`
+	Sub      string `json:"sub"`
+	Provider string `json:"provider"`
+	Status   int    `json:"status"`
 }
 
 type UserProfileResponse struct {
-    Sub      string `json:"sub"`
-    Provider string `json:"provider"`
-    Status   int    `json:"status"`
-    Login    string `json:"login,omitempty"`
-    Name     string `json:"name,omitempty"`
-    Email    string `json:"email,omitempty"`
+	Sub      string `json:"sub"`
+	Provider string `json:"provider"`
+	Status   int    `json:"status"`
+	Login    string `json:"login,omitempty"`
+	Name     string `json:"name,omitempty"`
+	Email    string `json:"email,omitempty"`
 }
 
 type GitHubUserAPIResponse struct {
-    ID    int64  `json:"id"`
-    Login string `json:"login"`
-    Email string `json:"email,omitempty"`
-    Name  string `json:"name,omitempty"`
+	ID    int64  `json:"id"`
+	Login string `json:"login"`
+	Email string `json:"email,omitempty"`
+	Name  string `json:"name,omitempty"`
 }
