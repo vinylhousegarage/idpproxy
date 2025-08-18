0d4e93949c3c22b1a20a6a4da731720d66df6e52
vinylhousegarage <vinylhousegarage@gmail.com>
2025-08-18 14:39:10 +0900

    test(github/user): rewrite api.github.com to mock server via custom RoundTripper in success test

--- test/internal/oauth/github/user_route_test.go
+++ test/internal/oauth/github/user_route_test.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 	"time"
 
@@ -16,28 +17,52 @@ import (
 	"github.com/vinylhousegarage/idpproxy/internal/oauth/github/user"
 )
 
+type rewriteRoundTripper struct {
+	target *url.URL
+	next   http.RoundTripper
+}
+
+func (r *rewriteRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	if req.URL.Host == "api.github.com" {
+		req.URL.Scheme = r.target.Scheme
+		req.URL.Host = r.target.Host
+	}
+	return r.next.RoundTrip(req)
+}
+
 func TestGitHubUserRoute_Success(t *testing.T) {
 	t.Parallel()
 
 	mockGitHub := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if r.URL.Path != "/user" {
-					http.NotFound(w, r)
-					return
-			}
-			w.Header().Set("Content-Type", "application/json")
-			_, _ = w.Write([]byte(`{"id":12345,"login":"octocat","name":"Mona","email":"mona@github.com"}`))
+		if r.URL.Path != "/user" {
+			http.NotFound(w, r)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		_, _ = w.Write([]byte(`{"id":12345,"login":"octocat","name":"Mona","email":"mona@github.com"}`))
 	}))
 	t.Cleanup(mockGitHub.Close)
 
+	u, err := url.Parse(mockGitHub.URL)
+	require.NoError(t, err)
+
+	client := &http.Client{
+		Timeout: time.Second,
+		Transport: &rewriteRoundTripper{
+			target: u,
+			next:   http.DefaultTransport,
+		},
+	}
+
 	apiCfg := &config.GitHubAPIConfig{
 		APIVersion: "2022-11-28",
-		BaseURL:    mockGitHub.URL,
+		BaseURL:    "https://api.github.com",
 		UserAgent:  "idpproxy-test",
 	}
-	deps := deps.NewGitHubAPIDeps(apiCfg, &http.Client{Timeout: time.Second}, zap.NewNop())
+	apiDeps := deps.NewGitHubAPIDeps(apiCfg, client, zap.NewNop())
 
 	r := gin.New()
-	r.GET("/user", user.NewGitHubUserHandler(deps))
+	r.GET("/user", user.NewGitHubUserHandler(apiDeps))
 
 	sut := httptest.NewServer(r)
 	t.Cleanup(sut.Close)
