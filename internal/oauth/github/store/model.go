597144470670db90498a8f7c2cd44260c9afd524
vinylhousegarage <vinylhousegarage@gmail.com>
2025-08-27 10:24:41 +0900

    feat(github/store): add GitHubTokenRecord struct to model.go

---
+++ internal/oauth/github/store/model.go
@@ -0,0 +1,17 @@
+package store
+
+type GitHubTokenRecord struct {
+    GitHubID     string    `firestore:"github_id"`
+    Provider     string    `firestore:"provider"`
+    FirebaseUID  string    `firestore:"firebase_uid"`
+    Login        string    `firestore:"login"`
+    Scopes       []string  `firestore:"scopes"`
+    TokenType    string    `firestore:"token_type"`
+    AccessToken  string    `firestore:"access_token"`
+    RefreshToken string    `firestore:"refresh_token"`
+    ExpiresAt    time.Time `firestore:"expires_at"`
+    LastUsedAt   time.Time `firestore:"last_used_at"`
+    CreatedAt    time.Time `firestore:"created_at"`
+    UpdatedAt    time.Time `firestore:"updated_at"`
+    DeleteAt     time.Time `firestore:"delete_at"`
+}
