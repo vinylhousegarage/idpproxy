1b04b38954427462efd5de8a73f6d10cb73029f6
vinylhousegarage <vinylhousegarage@gmail.com>
2025-09-09 15:01:43 +0900

    style(goimports): apply goimports to fix formatting error

--- internal/kms/client.go
+++ internal/kms/client.go
@@ -3,6 +3,7 @@ package kms
 import (
 	"context"
 	"errors"
+	"fmt"
 	"os"
 	"regexp"
 
@@ -16,14 +17,14 @@ var reSA = regexp.MustCompile(`^[a-zA-Z0-9\-]+@[a-z0-9\-]+\.iam\.gserviceaccount
 func NewClient(ctx context.Context) (*cloudkms.KeyManagementClient, error) {
 	if sa := os.Getenv("IMPERSONATE_SERVICE_ACCOUNT"); sa != "" {
 		if !reSA.MatchString(sa) {
-				return nil, fmt.Errorf("%w: invalid service account email %q", ErrInitFailed, sa)
+			return nil, fmt.Errorf("%w: invalid service account email %q", ErrInitFailed, sa)
 		}
 		ts, err := impersonate.CredentialsTokenSource(ctx, impersonate.CredentialsConfig{
-				TargetPrincipal: sa,
-				Scopes:          []string{"https://www.googleapis.com/auth/cloud-platform"},
+			TargetPrincipal: sa,
+			Scopes:          []string{"https://www.googleapis.com/auth/cloud-platform"},
 		})
 		if err != nil {
-				return nil, errors.Join(ErrInitFailed, err)
+			return nil, errors.Join(ErrInitFailed, err)
 		}
 		return cloudkms.NewKeyManagementClient(ctx, option.WithTokenSource(ts))
 	}
