30bae4597395a6677c7a7b1624a71236dd888011
vinylhousegarage <vinylhousegarage@gmail.com>
2025-09-09 15:00:15 +0900

    feat(kms): support Service Account Impersonation via IMPERSONATE_SERVICE_ACCOUNT

--- internal/kms/client.go
+++ internal/kms/client.go
@@ -3,16 +3,30 @@ package kms
 import (
 	"context"
 	"errors"
+	"os"
+	"regexp"
 
 	cloudkms "cloud.google.com/go/kms/apiv1"
+	"google.golang.org/api/impersonate"
 	"google.golang.org/api/option"
 )
 
-func NewClient(ctx context.Context, opts ...option.ClientOption) (*cloudkms.KeyManagementClient, error) {
-	client, err := cloudkms.NewKeyManagementClient(ctx, opts...)
-	if err != nil {
-		return nil, errors.Join(ErrInitFailed, err)
+var reSA = regexp.MustCompile(`^[a-zA-Z0-9\-]+@[a-z0-9\-]+\.iam\.gserviceaccount\.com$`)
+
+func NewClient(ctx context.Context) (*cloudkms.KeyManagementClient, error) {
+	if sa := os.Getenv("IMPERSONATE_SERVICE_ACCOUNT"); sa != "" {
+		if !reSA.MatchString(sa) {
+				return nil, fmt.Errorf("%w: invalid service account email %q", ErrInitFailed, sa)
+		}
+		ts, err := impersonate.CredentialsTokenSource(ctx, impersonate.CredentialsConfig{
+				TargetPrincipal: sa,
+				Scopes:          []string{"https://www.googleapis.com/auth/cloud-platform"},
+		})
+		if err != nil {
+				return nil, errors.Join(ErrInitFailed, err)
+		}
+		return cloudkms.NewKeyManagementClient(ctx, option.WithTokenSource(ts))
 	}
 
-	return client, nil
+	return cloudkms.NewKeyManagementClient(ctx)
 }
