1b8fe956cebb7a7289692dc899a2c381bc1671c2
vinylhousegarage <vinylhousegarage@gmail.com>
2025-09-11 07:54:28 +0900

    chore(config): remove unused constants from const.go

--- internal/config/const.go
+++ internal/config/const.go
@@ -1,27 +1,10 @@
 package config
 
 const (
-	// for fetching Google OIDC metadata
-	GoogleOIDCMetadataURL = "https://accounts.google.com/.well-known/openid-configuration"
-
-	// for GoogleConfig
-	GoogleResponseType = "code"
-	GoogleScope        = "openid email profile"
-	GoogleAccessType   = "offline"
-	GooglePrompt       = "consent"
-
-	// for GoogleTokenRepository
-	CollectionGoogleTokens = "google_tokens"
-
-	// for GenerateState
-	StateLength = 16
-
-	// for BuildGitHubLoginURL
-	GitHubAuthorizeURL = "https://github.com/login/oauth/authorize"
-
-	// for NewGitHubUserRequest
+	// for GitHub API
 	GitHubAPIBaseURL = "https://api.github.com"
-	GitHubUserURL    = GitHubAPIBaseURL + "/user"
 	GitHubAPIVersion = "2022-11-28"
+
+	// for UserAgent
 	UserAgentProduct = "idpproxy"
 )
