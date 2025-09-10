35c1b01426a2a7de7dc0339d776ec783128eb06a
vinylhousegarage <vinylhousegarage@gmail.com>
2025-09-11 08:23:14 +0900

    refactor(config): DRY GitHub OAuth loaders with prefix; add redirect URI validation; extract constants

--- internal/config/const.go
+++ internal/config/const.go
@@ -1,6 +1,10 @@
 package config
 
 const (
+	// for GitHub OAuth
+	GitHubAllowSignup = "true"
+	GitHubScope       = "read:user"
+
 	// for GitHub API
 	GitHubAPIBaseURL = "https://api.github.com"
 	GitHubAPIVersion = "2022-11-28"
