a191ec5f03ba840f54b15d9375bf1d0716c5a62c
vinylhousegarage <vinylhousegarage@gmail.com>
2025-09-11 09:30:20 +0900

    refactor(config): add missing constants(GitHubUserURL, GitHubAuthorizeURL, GoogleOIDCMetadataURL, StateLength)

--- internal/config/const.go
+++ internal/config/const.go
@@ -8,6 +8,16 @@ const (
 	// for GitHub API
 	GitHubAPIBaseURL = "https://api.github.com"
 	GitHubAPIVersion = "2022-11-28"
+	GitHubUserURL    = GitHubAPIBaseURL + "/user"
+
+	// for BuildGitHubLoginURL
+	GitHubAuthorizeURL = "https://github.com/login/oauth/authorize"
+
+	// for fetching Google OIDC metadata
+	GoogleOIDCMetadataURL = "https://accounts.google.com/.well-known/openid-configuration"
+
+	// for GenerateState
+	StateLength = 16
 
 	// for UserAgent
 	UserAgentProduct = "idpproxy"
