2514a096e2f1e6fddcd894ebf4095cbc7cc2de77
vinylhousegarage <vinylhousegarage@gmail.com>
2025-09-16 08:09:47 +0900

    feat(auth/store): add RefreshTokenRecord and AccessGenerationRecord structs

---
+++ internal/auth/store/refresh_token_redord.go
@@ -0,0 +1,21 @@
+package store
+
+import "time"
+
+type RefreshTokenRecord struct {
+	UserID     string `firestore:"user_id"`
+	PrevMacB64 string `firestore:"prev_mac_b64"`
+
+	CreatedAt  time.Time `firestore:"created_at"`
+	LastUsedAt time.Time `firestore:"last_used_at"`
+	ExpiresAt  time.Time `firestore:"expires_at"`
+	DeleteAt   time.Time `firestore:"delete_at"`
+
+	KMSMacKey string `firestore:"kms_mac_key"`
+}
+
+type AccessGenerationRecord struct {
+	UserID    string    `firestore:"user_id"`
+	Gen       int       `firestore:"gen"`
+	UpdatedAt time.Time `firestore:"updated_at"`
+}
