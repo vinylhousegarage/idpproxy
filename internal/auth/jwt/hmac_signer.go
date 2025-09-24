b0709af8d477be8ac14e5f6cd42801a209945881
vinylhousegarage <vinylhousegarage@gmail.com>
2025-09-24 13:56:43 +0900

    feat(auth/jwt): add Alg KeyID methods

--- internal/auth/jwt/hmac_signer.go
+++ internal/auth/jwt/hmac_signer.go
@@ -2,6 +2,8 @@ package jwt
 
 import "slices"
 
+const AlgHS256 = "HS256"
+
 type HMACSigner struct {
 	key   []byte
 	keyID string
@@ -14,3 +16,6 @@ func NewHMACSigner(key []byte, keyID string) *HMACSigner {
 		keyID: keyID,
 	}
 }
+
+func (s *HMACSigner) Alg() string   { return AlgHS256 }
+func (s *HMACSigner) KeyID() string { return s.keyID }
