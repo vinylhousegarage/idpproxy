c48f3480473152e2a360bd56b594ca805ef50f4b
vinylhousegarage <vinylhousegarage@gmail.com>
2025-09-26 16:05:48 +0900

    feat(auth/signer): add now field and Now() method to HMACSigner

--- internal/auth/signer/hmac_signer.go
+++ internal/auth/signer/hmac_signer.go
@@ -1,21 +1,34 @@
 package signer
 
-import "slices"
+import (
+	"slices"
+	"time"
+)
 
 const AlgHS256 = "HS256"
 
 type HMACSigner struct {
 	key   []byte
 	keyID string
+	now   func() time.Time
 }
 
 func NewHMACSigner(key []byte, keyID string) *HMACSigner {
-	keyCopy := slices.Clone(key)
 	return &HMACSigner{
-		key:   keyCopy,
+		key:   slices.Clone(key),
 		keyID: keyID,
+		now:   time.Now,
 	}
 }
 
-func (s *HMACSigner) Alg() string   { return AlgHS256 }
+func (s *HMACSigner) Alg() string { return AlgHS256 }
+
 func (s *HMACSigner) KeyID() string { return s.keyID }
+
+func (s *HMACSigner) Now() time.Time {
+	if s.now != nil {
+		return s.now()
+	}
+
+	return time.Now()
+}
