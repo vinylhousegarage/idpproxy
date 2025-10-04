a15e72b38814878f4d5cd75d4fbce416fa67e2be
vinylhousegarage <vinylhousegarage@gmail.com>
2025-10-04 12:19:10 +0900

    feat(auth/store): feat(auth/store): add mapNotFound helper for ErrNotFound handling

---
+++ internal/auth/store/firestore_errors.go
@@ -0,0 +1,13 @@
+package store
+
+import (
+    "google.golang.org/grpc/codes"
+    "google.golang.org/grpc/status"
+)
+
+func mapNotFound(err error) error {
+    if status.Code(err) == codes.NotFound {
+        return ErrNotFound
+    }
+    return err
+}
