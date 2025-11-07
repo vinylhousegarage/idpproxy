a7fc4a036e3f17998cc1136859fead71b4f29a71
vinylhousegarage <vinylhousegarage@gmail.com>
2025-11-07 14:33:41 +0900

    feat(oauth/github): define package-level errors for GitHub token service

---
+++ internal/oauth/github/callback/errors.go
@@ -0,0 +1,9 @@
+package github
+
+import "errors"
+
+var (
+	ErrInvalidInput = errors.New("github: invalid input")
+	ErrEncrypt      = errors.New("github: encrypt failed")
+	ErrPersist      = errors.New("github: persist failed")
+)
