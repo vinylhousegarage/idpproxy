50c26b62322485d4e835d3804848ba49f1600184
vinylhousegarage <vinylhousegarage@gmail.com>
2025-11-27 06:56:19 +0900

    chore(auth/session): move usecase errors to errors.go and group by responsibility

---
+++ internal/auth/session/errors.go
@@ -0,0 +1,10 @@
+package session
+
+import "errors"
+
+// Usecase validation
+var (
+	ErrEmptySessionID       = errors.New("session: empty sessionID")
+	ErrEmptyUserID          = errors.New("session: empty userID")
+	ErrInvalidUsecaseConfig = errors.New("session: invalid usecase configuration")
+)
