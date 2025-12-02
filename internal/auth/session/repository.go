34918b39b709b2fd10522f52d31ccff204454cdc
vinylhousegarage <vinylhousegarage@gmail.com>
2025-12-02 13:47:20 +0900

    feat(auth/session): add Update method to Repository interface

--- internal/auth/session/repository.go
+++ internal/auth/session/repository.go
@@ -7,4 +7,5 @@ import (
 type Repository interface {
 	Create(ctx context.Context, s *Session) error
 	FindByID(ctx context.Context, sessionID string) (*Session, error)
+	Update(ctx context.Context, s *Session) error
 }
