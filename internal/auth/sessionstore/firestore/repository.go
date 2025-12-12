8b1704cfea9dcb0eebb1ddeb72091526bb4f5e70
vinylhousegarage <vinylhousegarage@gmail.com>
2025-12-12 12:57:53 +0900

    feat(auth/sessionstore/firestore): add FindByID repository method

--- internal/auth/sessionstore/firestore/repository.go
+++ internal/auth/sessionstore/firestore/repository.go
@@ -5,6 +5,8 @@ import (
 
 	"cloud.google.com/go/firestore"
 	"github.com/vinylhousegarage/idpproxy/internal/auth/session"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type Repository struct {
@@ -24,4 +26,21 @@ func NewRepository(client *firestore.Client, collectionName string) *Repository 
 func (r *Repository) Create(ctx context.Context, s *session.Session) error {
 	_, err := r.collection.Doc(s.SessionID).Set(ctx, s)
 	return err
+}
+
+func (r *Repository) FindByID(ctx context.Context, sessionID string) (*session.Session, error) {
+	doc, err := r.collection.Doc(sessionID).Get(ctx)
+	if err != nil {
+		if status.Code(err) == codes.NotFound {
+			return nil, session.ErrNotFound
+		}
+		return nil, err
+	}
+
+	var s session.Session
+	if err := doc.DataTo(&s); err != nil {
+		return nil, err
+	}
+
+	return &s, nil
 }
