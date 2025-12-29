d43d3bd38044a5a77b10b23e4b7bc3b4731c06ef
vinylhousegarage <vinylhousegarage@gmail.com>
2025-12-29 13:47:34 +0900

    feat(authcode/service): add Issue service method

--- internal/authcode/service/service.go
+++ internal/authcode/service/service.go
@@ -1,7 +1,29 @@
 package service
 
-import "github.com/vinylhousegarage/idpproxy/internal/authcode/store"
+import (
+	"context"
+	"time"
+
+	"github.com/vinylhousegarage/idpproxy/internal/authcode"
+	"github.com/vinylhousegarage/idpproxy/internal/authcode/store"
+)
 
 type Service struct {
 	store store.Store
+}
+
+func (s *Service) Issue(
+	ctx context.Context,
+	userID string,
+	clientID string,
+) error {
+
+	code := authcode.AuthCode{
+		Code:      "dummy",
+		UserID:    userID,
+		ClientID:  clientID,
+		ExpiresAt: time.Now().Add(5 * time.Minute),
+	}
+
+	return s.store.Save(ctx, code)
 }
