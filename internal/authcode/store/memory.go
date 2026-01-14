475dcb3415180df0b107f0e0de3e186138687654
vinylhousegarage <vinylhousegarage@gmail.com>
2026-01-14 11:55:56 +0900

    feat(authcode/store): add Consume method

--- internal/authcode/store/memory.go
+++ internal/authcode/store/memory.go
@@ -3,6 +3,7 @@ package store
 import (
 	"context"
 	"sync"
+	"time"
 
 	"github.com/vinylhousegarage/idpproxy/internal/authcode"
 )
@@ -24,4 +25,26 @@ func (s *MemoryStore) Save(ctx context.Context, code authcode.AuthCode) error {
 
 	s.codes[code.Code] = code
 	return nil
+}
+
+func (s *MemoryStore) Consume(ctx context.Context, code, clientID string) (string, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	ac, ok := s.codes[code]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	delete(s.codes, code)
+
+	if ac.ClientID != clientID {
+		return "", ErrClientMismatch
+	}
+
+	if time.Now().After(ac.ExpiresAt) {
+		return "", ErrExpired
+	}
+
+	return ac.UserID, nil
 }
