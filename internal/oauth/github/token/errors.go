e94cb73d5a0f97a8dd1d6e26f252dfe344401206
vinylhousegarage <vinylhousegarage@gmail.com>
2026-01-27 11:40:49 +0900

    refactor(github/token): fix error message prefix

--- internal/oauth/github/token/errors.go
+++ internal/oauth/github/token/errors.go
@@ -3,9 +3,9 @@ package token
 import "errors"
 
 var (
-	ErrEmptyBody          = errors.New("callback: empty response body")
-	ErrGitHubOAuthError   = errors.New("callback: github oauth error")
-	ErrMissingAccessToken = errors.New("callback: missing access_token in response")
-	ErrNon2xxStatus       = errors.New("callback: non-2xx response")
-	ErrParseFormBody      = errors.New("callback: parse form body")
+	ErrEmptyBody          = errors.New("token: empty response body")
+	ErrGitHubOAuthError   = errors.New("token: github oauth error")
+	ErrMissingAccessToken = errors.New("token: missing access_token in response")
+	ErrNon2xxStatus       = errors.New("token: non-2xx response")
+	ErrParseFormBody      = errors.New("token: parse form body")
 )
