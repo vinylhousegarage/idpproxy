e84d88f6b9bc53595a8a007b146342fb0e129e59
vinylhousegarage <vinylhousegarage@gmail.com>
2026-02-03 11:38:15 +0900

    feat(idpproxy/token): add TokenResponse model for token endpoint

---
+++ internal/oauth/idpproxy/response.go
@@ -0,0 +1,8 @@
+package token
+
+type TokenResponse struct {
+	AccessToken  string `json:"access_token"`
+	TokenType    string `json:"token_type"`
+	ExpiresIn    int64  `json:"expires_in"`
+	RefreshToken string `json:"refresh_token,omitempty"`
+}
