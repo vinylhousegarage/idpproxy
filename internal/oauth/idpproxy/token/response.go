2d76b1b66f33543387f4c70b1f26a34ef91e49d8
vinylhousegarage <vinylhousegarage@gmail.com>
2026-02-12 11:08:35 +0900

    refactor(idpproxy/token): change TokenResponse to id_token-only structure

--- internal/oauth/idpproxy/token/response.go
+++ internal/oauth/idpproxy/token/response.go
@@ -1,8 +1,5 @@
 package token
 
 type TokenResponse struct {
-	AccessToken  string `json:"access_token"`
-	TokenType    string `json:"token_type"`
-	ExpiresIn    int64  `json:"expires_in"`
-	RefreshToken string `json:"refresh_token,omitempty"`
+	IDToken string `json:"id_token"`
 }
