ce76eb22c3a730b363c8b40dccd8fd083336d9e8
vinylhousegarage <vinylhousegarage@gmail.com>
2026-02-14 10:45:12 +0900

    refactor(idpproxy/token): remove unused ErrExpiredCode

--- internal/oauth/idpproxy/token/errors.go
+++ internal/oauth/idpproxy/token/errors.go
@@ -3,7 +3,6 @@ package token
 import "errors"
 
 var (
-	ErrExpiredCode          = errors.New("token: expired auth code")
 	ErrInvalidClient        = errors.New("token: invalid client")
 	ErrInvalidGrant         = errors.New("token: invalid grant")
 	ErrUnsupportedGrantType = errors.New("token: unsupported grant_type")
