0cee200a80739f60eb6060ab97c991235b17615e
vinylhousegarage <vinylhousegarage@gmail.com>
2026-03-02 15:20:12 +0900

    refactor(github/callback): rename AuthCodeService to ProxyCodeService

--- internal/oauth/github/callback/ports.go
+++ internal/oauth/github/callback/ports.go
@@ -8,6 +8,6 @@ type UserService interface {
 	UpsertFromGitHub(ctx context.Context, ghID int64, login, email string) (string, error)
 }
 
-type AuthCodeService interface {
+type ProxyCodeService interface {
 	Issue(ctx context.Context, userID string, clientID string) (string, error)
 }
