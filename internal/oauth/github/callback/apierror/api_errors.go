4be246777baa6b429bcb2b42feaf040899c32312
vinylhousegarage <vinylhousegarage@gmail.com>
2026-05-20 07:51:26 +0900

    refactor(github/apierror): support internal message in APIError and factory functions

--- internal/oauth/github/callback/apierror/api_errors.go
+++ internal/oauth/github/callback/apierror/api_errors.go
@@ -2,22 +2,22 @@ package apierror
 
 import "net/http"
 
-func MissingGitHubCode(err error) *APIError {
-	return New(ErrorMissingGitHubCode, http.StatusBadRequest, err)
+func MissingGitHubCode(err error, internal ...string) *APIError {
+	return New(ErrorMissingGitHubCode, http.StatusBadRequest, err, internal...)
 }
 
-func MissingState(err error) *APIError {
-	return New(ErrorMissingState, http.StatusBadRequest, err)
+func MissingState(err error, internal ...string) *APIError {
+	return New(ErrorMissingState, http.StatusBadRequest, err, internal...)
 }
 
-func InvalidState(err error) *APIError {
-	return New(ErrorInvalidState, http.StatusBadRequest, err)
+func InvalidState(err error, internal ...string) *APIError {
+	return New(ErrorInvalidState, http.StatusBadRequest, err, internal...)
 }
 
-func ProxyCodeIssue(err error) *APIError {
-	return New(ErrorProxyCodeIssue, http.StatusInternalServerError, err)
+func ProxyCodeIssue(err error, internal ...string) *APIError {
+	return New(ErrorProxyCodeIssue, http.StatusInternalServerError, err, internal...)
 }
 
-func Internal(err error) *APIError {
-	return New(ErrorInternal, http.StatusInternalServerError, err)
+func Internal(err error, internal ...string) *APIError {
+	return New(ErrorInternal, http.StatusInternalServerError, err, internal...)
 }
