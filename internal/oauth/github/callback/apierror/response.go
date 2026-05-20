78451031abd2f6e55db8d9a4c6e16c05c2d9f826
vinylhousegarage <vinylhousegarage@gmail.com>
2026-05-21 06:51:03 +0900

    feat(github/callback/apierror): add Respond function to response.go

--- internal/oauth/github/callback/apierror/response.go
+++ internal/oauth/github/callback/apierror/response.go
@@ -1,5 +1,13 @@
 package apierror
 
+import "github.com/gin-gonic/gin"
+
 type ErrorResponse struct {
 	Error ErrorCode `json:"error"`
+}
+
+func Respond(c *gin.Context, apiErr *APIError) {
+	_ = c.Error(apiErr)
+
+	c.JSON(apiErr.HTTPStatus, gin.H{"error": apiErr.Code})
 }
