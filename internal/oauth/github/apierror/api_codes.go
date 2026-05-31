ea9aa7fccc426f4767344ff2c00244485b9546d8
vinylhousegarage <vinylhousegarage@gmail.com>
2026-06-01 02:03:29 +0900

    refactor(github/apierror): merge api_codes.go and api_errors.go into errors.go

--- internal/oauth/github/apierror/api_codes.go
+++
@@ -1,7 +0,0 @@
-package apierror
-
-const (
-	ErrorCodeInternal       ErrorCode = "internal_error"
-	ErrorCodeProxyCodeIssue ErrorCode = "proxy_code_issue_failed"
-	ErrorCodeUserUpsert     ErrorCode = "user_upsert_failed"
-)
