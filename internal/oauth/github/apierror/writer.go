2b67cb123c206b1a44a20507994f5cc0e1640f09
vinylhousegarage <vinylhousegarage@gmail.com>
2026-07-14 05:55:00 +0900

    refactor(github/apierror): use GetHTTPStatus() for HTTP status access

--- internal/oauth/github/apierror/writer.go
+++ internal/oauth/github/apierror/writer.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func WriteError(c *gin.Context, apiErr *APIError) {
 	defer c.Abort()
 
-	c.JSON(apiErr.HTTPStatus, ErrorResponse{
+	c.JSON(apiErr.GetHTTPStatus(), ErrorResponse{
 		Error: apiErr.Code,
 	})
 }
